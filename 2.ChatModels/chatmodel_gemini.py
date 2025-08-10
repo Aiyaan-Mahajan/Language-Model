b8cf0d3060c5497a923a2ff4dd5e2ed1bc3d4d47
Aiyaan Mahajan <aiyaanmahajan@gmail.com>
2025-08-10 06:49:43 +0530

    Update chatmodel_gemini.py

--- 2.ChatModels/chatmodel_gemini.py
+++ 2.ChatModels/chatmodel_gemini.py
@@ -1,8 +1,9 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI #basechatmodels class
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model='gemini-2.5-pro')
+model = ChatGoogleGenerativeAI(model='gemini-2.5-pro')#instance of basechatmodels class  and along with models you can also include temperature and max_token_limit .
+#temperature should be 0 if you want things like code and if you want to do something creative it must be somewhere around 1.5 .
 result = model.invoke('Who is the president of India')
 print(result.content)
